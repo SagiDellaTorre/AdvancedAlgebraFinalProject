9872000c89f06884b228080bbb9111d77d303f97
amiteli <amiteliav@gmail.com>
2023-05-17 13:06:05 +0300

    add comments and little more tests

--- FiniteField.py
+++ FiniteField.py
@@ -1,4 +1,5 @@
 import galois
+import numpy as np
 
 class FiniteField:
     """
@@ -8,5 +9,16 @@ class FiniteField:
     """
     # constructor
     def __init__(self, p, fx):
+        """
+
+        :param p:  a prime number
+        :param fx: a list of polynomial's coefficients.
+                   given in common printed form a_n * x^n + ... + a_1 * x + a_0
+                   and as a list of coefficients [a_0, a_1, ...]. Note the order.
+        """
         self.p = p      # assume that p is indeed prime
         self.fx = fx    # assume that fx is indeed irreducible
+
+        # make the poly' a Monic polynomial (so a_n=1)
+        # TODO: the coefficients should be in F. do we make use they are?
+        self.fx = (np.array(self.fx) / self.fx[-1]).tolist()
